26f11e4c2a9214fda235ce4868244b9c78c2c6e3
bilal-fazlani <bilal.m.fazlani@gmail.com>
2017-04-15 01:34:40 +0530

    Added a small cosole app to use logic and generate files

---
+++ ExcelToSQLScripts.Console/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using ExcelToSQLScripts.Models;
+using static System.Console;
+
+namespace ExcelToSQLScripts.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputPath = GetInput("Enter excel file path").Replace("\"","");
+            string outputPath = GetInput("Enter output directory path").Replace("\"", "");
+
+            TableScriptGenerator tableScriptGenerator = new TableScriptGenerator(new QueryMaker());
+
+            ExcelReader excelReader = new ExcelReader();
+
+            IEnumerable<Table> tables = excelReader.Read(inputPath);
+
+            foreach (Table table in tables)
+            {
+                WriteLine($"{table.Name}.sql...");
+
+                using (Script script = tableScriptGenerator.GenerateTableScript(table))
+                using (FileStream fileStream = File.Create(Path.Combine(outputPath, script.Name + ".sql")))
+                {
+                    script.Content.CopyTo(fileStream);
+                    Write(" done");
+                }
+            }
+        }
+
+        private static string GetInput(string text)
+        {
+            WriteLine(text);
+            string path = ReadLine();
+            return string.IsNullOrEmpty(path) ? GetInput(text) : path;
+        }
+    }
+}
