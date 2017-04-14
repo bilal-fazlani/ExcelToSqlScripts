9312239950167401fbe8e6cb1ca5620355de8953
bilal-fazlani <bilal.m.fazlani@gmail.com>
2017-04-15 02:50:25 +0530

    Added command line argument parsing

--- ExcelToSQLScripts.Console/Program.cs
+++ ExcelToSQLScripts.Console/Program.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using ExcelToSQLScripts.Models;
+using Microsoft.Extensions.CommandLineUtils;
 using static System.Console;
 
 namespace ExcelToSQLScripts.Console
@@ -9,31 +11,56 @@ namespace ExcelToSQLScripts.Console
     {
         static void Main(string[] args)
         {
-            string inputPath = GetInput("Enter excel file path").Replace("\"","");
-            string outputPath = GetInput("Enter output directory path").Replace("\"", "");
+            var app = new CommandLineApplication();
 
-            TableScriptGenerator tableScriptGenerator = new TableScriptGenerator(new QueryMaker());
+            //give people help with --help
+            app.HelpOption("-? | -h | --help");
 
-            ExcelReader excelReader = new ExcelReader();
+            var inputArguement = app.Argument("inputFile", "Full path of excel file. File must have .xlsx extension.");
+            var outputArguement = app.Argument("outputDirectory", "Path the the directory where all sql files will be stored. " +
+                                                                  "If one or more files exist with same name, they will be overriden. " +
+                                                                  "If output directory doesn't exist, it will be created.");
 
-            IEnumerable<Table> tables = excelReader.Read(inputPath);
-
-            foreach (Table table in tables)
+            app.OnExecute(() =>
             {
-                WriteLine($"{table.Name}.sql...");
+                string inputPath = inputArguement.Value ?? GetInput("Enter excel file path: ").Replace("\"", "");
+                string outputPath =outputArguement.Value ?? GetInput("Enter output directory path: ").Replace("\"", "");
 
-                using (Script script = tableScriptGenerator.GenerateTableScript(table))
-                using (FileStream fileStream = File.Create(Path.Combine(outputPath, script.Name + ".sql")))
+                TableScriptGenerator tableScriptGenerator = new TableScriptGenerator(new QueryMaker());
+
+                ExcelReader excelReader = new ExcelReader();
+
+                try
                 {
-                    script.Content.CopyTo(fileStream);
-                    Write(" done");
+                    IEnumerable<Table> tables = excelReader.Read(inputPath);
+                    foreach (Table table in tables)
+                    {
+                        WriteLine($"{table.Name}.sql...");
+
+                        using (Script script = tableScriptGenerator.GenerateTableScript(table))
+                        using (FileStream fileStream = File.Create(Path.Combine(outputPath, script.Name + ".sql")))
+                        {
+                            script.Content.CopyTo(fileStream);
+                            Write(" done");
+                        }
+                    }
+
+                    return 0;
                 }
-            }
+                catch (FileNotFoundException ex)
+                {
+                    WriteLine($"file not found:  {ex.FileName}");
+                    return 1;
+                }
+            });
+
+            var result = app.Execute(args);
+            Environment.Exit(result);
         }
 
         private static string GetInput(string text)
         {
-            WriteLine(text);
+            Write(text);
             string path = ReadLine();
             return string.IsNullOrEmpty(path) ? GetInput(text) : path;
         }
