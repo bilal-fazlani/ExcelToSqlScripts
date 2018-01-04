a11f894754242b528da6268fe48bb5941042950d
Bilal Fazlani <bilal_fazlani@external.mckinsey.com>
2018-01-04 22:18:30 +0530

    Started using commanddotnet

---
+++ ExcelToSQLScripts.Console/App.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using CommandDotNet.Attributes;
+using ExcelToSQLScripts.Models;
+using ExcelToSQLScripts.QueryMakers;
+
+using static System.Console;
+
+namespace ExcelToSQLScripts.Console
+{
+    public class App
+    {
+        private readonly string _inputFile;
+        private readonly string _outputDirectory;
+        private readonly List<int> _worksheetsToRead;
+        private readonly bool _readEmptyRecords;
+        private readonly List<string> _nullReplacements;
+
+        public App([Option(Inherited = true, ShortName = "i", LongName = "inputFile", Description = "Full path of excel file. File must have .xlsx extension.")]string inputFile,
+            [Option(Inherited = true, ShortName = "o", LongName = "outputDirectory", Description = "Path the the directory where all sql files will be stored. " +
+                                                                                 "If one or more files exist with same name, they will be overriden. " +
+                                                                                 "If output directory doesn't exist, it will be created.")] string outputDirectory,
+            [Option(Inherited = true, ShortName = "w", LongName = "worksheet", Description = "Index of worksheets to be processed. Index beings from 1. This option can be used multiple times in one command.")] List<int> worksheetsToRead,
+            [Option(Inherited = true, ShortName = "e", LongName = "insertEmptyRecords", Description = "Will insert NULLs in all fields for empty rows in excel sheet")] bool readEmptyRecords,
+            [Option(Inherited = true, ShortName = "r", LongName = "replaceWithNULL", Description = "Replace the given text with null values in script. This option can be used multiple times in one command.")] List<string> nullReplacements)
+        {
+            _inputFile = inputFile;
+            _outputDirectory = outputDirectory;
+            _worksheetsToRead = worksheetsToRead;
+            _readEmptyRecords = readEmptyRecords;
+            _nullReplacements = nullReplacements;   
+        }
+        
+        [ApplicationMetadata(Description = "Generates insert scripts")]
+        public int Insert()
+        {
+            return Process("insert");
+        }
+
+        [ApplicationMetadata(Description = "Generates update scripts")]
+        public int Update()
+        {
+            return Process("update");
+        }
+
+        [ApplicationMetadata(Description = "Generates merge scripts")]
+        public int Merge()
+        {
+            return Process("merge");
+        }
+
+        private int Process(string mode)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(_inputFile))
+                    throw new ArgumentNullException(nameof(_inputFile), "Please provide a path to excel file");
+
+                if (string.IsNullOrEmpty(_outputDirectory))
+                    throw new ArgumentNullException(nameof(_outputDirectory),
+                        "Please provide a directory for saving sql scripts");
+
+                Directory.CreateDirectory(_outputDirectory);
+
+                ExcelReader excelReader = new ExcelReader(_readEmptyRecords, _worksheetsToRead?.ToArray());
+
+                ValueRenderer valueRenderer = new ValueRenderer(_nullReplacements?.ToArray());
+            
+                IQueryMaker queryMaker = QueryMakerFactory.Create(mode, valueRenderer);
+
+                TableScriptGenerator tableScriptGenerator = new TableScriptGenerator(queryMaker);
+
+                IEnumerable<Table> tables = excelReader.Read(_inputFile);
+
+                foreach (Table table in tables)
+                {
+                    string filePath = Path.Combine(_outputDirectory, table.Name + ".sql");
+                    Write($"writing {filePath} ...");
+
+                    if (table.Records.Any())
+                    {
+                        using (Script script = tableScriptGenerator.GenerateTableScript(table))
+                        {
+                            using (FileStream fileStream = File.Create(filePath))
+                            {
+                                script.Content.CopyTo(fileStream);
+                                WriteLine(" done");
+                            }
+                        }
+                    }
+                    else
+                    {
+                        WriteLine(" empty (skipped)");
+                    }
+                }
+
+                return 0;
+            }
+            catch (FileNotFoundException ex)
+            {
+                Error.WriteLine($"file not found:  {ex.FileName}");
+#if DEBUG
+                Error.WriteLine(ex.StackTrace);
+#endif 
+                return 1;
+            }
+            catch (Exception ex)
+            {
+                Error.WriteLine($"Error: {ex.Message}");
+#if DEBUG
+                Error.WriteLine(ex.StackTrace);
+#endif 
+                return 1;
+            }
+        }
+    }
+}
--- ExcelToSQLScripts.Console/Program.cs
+++ ExcelToSQLScripts.Console/Program.cs
@@ -1,180 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using ExcelToSQLScripts.Models;
-using ExcelToSQLScripts.QueryMakers;
-using Microsoft.Extensions.CommandLineUtils;
-using static System.Console;
+﻿using CommandDotNet;
+using CommandDotNet.Models;
 
 namespace ExcelToSQLScripts.Console
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            CommandLineApplication app = new CommandLineApplication();
-
-            app.HelpOption("-? | -h | --help");
-
-            app.Command("insert", command =>
+            AppRunner<App> appRunner = new AppRunner<App>(new AppSettings
             {
-                command.Description = "Generates insert scripts";
-
-                CommandOption inputOption = HydrateCommandWithOptions(command, out CommandOption outputOption,
-                    out CommandOption nullRecordOption,
-                    out CommandOption workSheetsOption, out CommandOption replacementOption);
-
-                command.OnExecute(() => GenerateScripts(inputOption, outputOption, nullRecordOption, workSheetsOption,
-                    replacementOption, command, "insert"));
+                Case = Case.CamelCase,
+                ShowArgumentDetails = false
             });
 
-            app.Command("update", command =>
-            {
-                command.Description = "Generates update scripts";
-
-                CommandOption inputOption = HydrateCommandWithOptions(command, out CommandOption outputOption,
-                    out CommandOption nullRecordOption,
-                    out CommandOption workSheetsOption, out CommandOption replacementOption);
-
-                command.OnExecute(() => GenerateScripts(inputOption, outputOption, nullRecordOption, workSheetsOption,
-                    replacementOption, command, "update"));
-            });
-
-            app.Command("merge", command =>
-            {
-                command.Description = "Generates merge scripts";
-
-                CommandOption inputOption = HydrateCommandWithOptions(command, out CommandOption outputOption,
-                    out CommandOption nullRecordOption,
-                    out CommandOption workSheetsOption, out CommandOption replacementOption);
-
-                command.OnExecute(() => GenerateScripts(inputOption, outputOption, nullRecordOption, workSheetsOption,
-                    replacementOption, command, "merge"));
-            });
-
-            app.OnExecute(() =>
-            {
-                app.ShowHelp();
-                return 0;
-            });
-
-            try
-            {
-                int result = app.Execute(args);
-                Environment.Exit(result);
-            }
-            catch (CommandParsingException e)
-            {
-                WriteLine(e.Message);
-                app.ShowHelp();
-                Environment.Exit(1);
-            }
-        }
-
-        private static CommandOption HydrateCommandWithOptions(CommandLineApplication command,
-            out CommandOption outputOption,
-            out CommandOption nullRecordOption, out CommandOption workSheetsOption, out CommandOption replacementOption)
-        {
-            command.HelpOption("-? | -h | --help");
-
-            CommandOption inputOption = command.Option("-i | --inputFile",
-                "Full path of excel file. File must have .xlsx extension.", CommandOptionType.SingleValue);
-
-            outputOption = command.Option("-o | --outputDirectory",
-                "Path the the directory where all sql files will be stored. " +
-                "If one or more files exist with same name, they will be overriden. " +
-                "If output directory doesn't exist, it will be created.", CommandOptionType.SingleValue);
-
-            nullRecordOption = command.Option("-e | --insertEmptyRecords",
-                "Will insert NULLs in all fields for empty rows in excel sheet", CommandOptionType.NoValue);
-
-            workSheetsOption = command.Option("-w | --worksheet",
-                "Index of worksheets to be processed. Index beings from 1. This option can be used multiple times in one command.",
-                CommandOptionType.MultipleValue);
-
-            replacementOption = command.Option("-r | --replaceWithNULL",
-                "Replace the given text with null values in script. This option can be used multiple times in one command.",
-                CommandOptionType.MultipleValue);
-            return inputOption;
-        }
-
-        private static int GenerateScripts(CommandOption inputOption, CommandOption outputOption,
-            CommandOption nullRecordOption, CommandOption workSheetsOption,
-            CommandOption replacementOption, CommandLineApplication command, string mode)
-        {
-            try
-            {
-                string inputPath = inputOption.Value() ??
-                                   throw new ArgumentNullException("inputFile",
-                                       "Please provide a path to excel file");
-                string outputPath = outputOption.Value() ?? throw new ArgumentNullException("outputDirectory",
-                                        "Please provide a directory for saving sql scripts");
-                bool readEmptyRecords = nullRecordOption.HasValue() && nullRecordOption.Value() == "on";
-
-                int[] worksheetsToRead = workSheetsOption.Values?.Select(int.Parse).ToArray();
-
-                string[] nullReplacements = replacementOption.Values?.ToArray();
-
-                Directory.CreateDirectory(outputPath);
-
-                ExcelReader excelReader = new ExcelReader(readEmptyRecords, worksheetsToRead);
-
-                ValueRenderer valueRenderer = new ValueRenderer(nullReplacements);
-
-                IQueryMaker queryMaker = QueryMakerFactory.Create(mode, valueRenderer);
-
-                TableScriptGenerator tableScriptGenerator = new TableScriptGenerator(queryMaker);
-
-                IEnumerable<Table> tables = excelReader.Read(inputPath);
-
-                foreach (Table table in tables)
-                {
-                    string filePath = Path.Combine(outputPath, table.Name + ".sql");
-                    Write($"writing {filePath} ...");
-
-                    if (table.Records.Any())
-                    {
-                        using (Script script = tableScriptGenerator.GenerateTableScript(table))
-                        {
-                            using (FileStream fileStream = File.Create(filePath))
-                            {
-                                script.Content.CopyTo(fileStream);
-                                WriteLine(" done");
-                            }
-                        }
-                    }
-                    else
-                    {
-                        WriteLine(" empty (skipped)");
-                    }
-                }
-
-                return 0;
-            }
-            catch (ArgumentNullException ex)
-            {
-                Error.WriteLine($"Error: {ex.Message}");
-                command.ShowHelp();
-                return 1;
-            }
-            catch (ArgumentException ex)
-            {
-                Error.WriteLine($"Error: {ex.Message}");
-                command.ShowHelp();
-                return 1;
-            }
-            catch (FileNotFoundException ex)
-            {
-                Error.WriteLine($"file not found:  {ex.FileName}");
-                return 1;
-            }
-            catch (Exception ex)
-            {
-                Error.WriteLine($"Error: {ex.Message}");
-                Error.WriteLine(ex.StackTrace);
-                return 1;
-            }
+            return appRunner.Run(args);
         }
     }
 }
